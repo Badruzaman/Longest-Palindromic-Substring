98224c0a288563f8f4452888f39dacd2ef49b992
badruzzaman <badruzzaman@DESKTOP-EHGT2P0>
2022-09-11 09:50:42 +0600

    Add project files.

---
+++ LongestPalindromicSubstring/Program.cs
@@ -0,0 +1,41 @@
+﻿
+string str = "abaxyzzyxf";
+
+string result = LongestPalindromicSubsting(str);
+Console.WriteLine(result);
+Console.ReadKey();
+
+
+string LongestPalindromicSubsting(string str)
+{
+    string longest = string.Empty;
+    for (int i = 0; i < str.Length; i++)
+    {
+        for (int j = i; j < str.Length; j++)
+        {
+            string substring = str.Substring(i, j + 1 - i);
+            if(longest.Length < substring.Length && IsPalindrome(substring))
+            {
+                longest = substring;
+            }
+        }
+    }
+    return longest;
+}
+
+bool IsPalindrome(string str)
+{
+    int left = 0;
+    int right = str.Length - 1;
+    while (left < right)
+    {
+        if (str[left] != str[right])
+        {
+            return false;
+        }
+        left++;
+        right--;
+    }
+    return true;
+}
+
